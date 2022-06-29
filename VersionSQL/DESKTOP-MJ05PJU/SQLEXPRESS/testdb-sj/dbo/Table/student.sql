169ea76c1316712a9af1f241d300fc62c9c5d27f
songjiesj <41512051+songjiesj@users.noreply.github.com>
2022-06-29 10:28:07 +0800

    first-commit

---
+++ VersionSQL/DESKTOP-MJ05PJU/SQLEXPRESS/testdb-sj/dbo/Table/student.sql
@@ -0,0 +1,9 @@
+﻿/****** Object:  Table [dbo].[student]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[student](
+	[name] [nchar](10) NOT NULL,
+	[age] [int] NULL,
+	[grade] [text] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
