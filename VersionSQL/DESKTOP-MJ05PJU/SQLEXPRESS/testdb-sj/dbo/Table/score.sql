ea74090b7e18396f9187fcbc29364ba1ba8d7dcd
songjiesj <41512051+songjiesj@users.noreply.github.com>
2022-06-29 10:49:01 +0800

    commit entire db

---
+++ VersionSQL/DESKTOP-MJ05PJU/SQLEXPRESS/testdb-sj/dbo/Table/score.sql
@@ -0,0 +1,10 @@
+﻿/****** Object:  Table [dbo].[score]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[score](
+	[subject] [nchar](10) NULL,
+	[name] [nchar](10) NULL,
+	[id] [nchar](10) NULL,
+	[score] [nchar](10) NULL
+) ON [PRIMARY]
